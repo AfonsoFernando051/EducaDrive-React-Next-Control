1aa5e498d9179f01555b3e9711fcfdf17c349a50
AfonsoFernando051 <afonsofernando051@gmail.com>
2023-09-29 13:20:02 -0300

    formulário de cadastro ok

--- src/pages/Cadastrar.jsx
+++ src/pages/Cadastrar.jsx
@@ -1,31 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import '../app//styles/globals.css'
 import Header from "@/componentes/Header";
 import Home from "@/app/page";
+import Image from "next/image";
+import { useState } from 'react';
 
 const Cadastrar = function(){
     
     return(
         <>
         <Home>
-            <form>
-                <h2>Criar Login</h2>
-                <div className="SignUpForm">
-                    <div>
-                        <input type="text" placeholder="Nome"/>
+            <div id="cadastrarPag">
+                <form>
+                    <div id="titleForm">
+                        <h1>Cadastro de autoescolas clientes</h1>
                     </div>
-                    <div>
-                        <input type="text" placeholder="Email"/>
+                    <div className="SignUpForm">
+                            <h2>Insira os dados de login:</h2>
+                        <div>
+                            <input type="text" placeholder="name" className="inputForm"/>
+                        </div>
+                        <div>
+                            <input type="email" placeholder="email" className="inputForm"/>
+                        </div>
+                        <div>
+                            <input type="password" placeholder="password" className="inputForm"/>
+                        </div>
+                        <div>
+                            <input type="radio" name="acess" id="acess" />
+                            <span>Permissão de acesso</span>
+                        </div>
+                        <div id="buttonForm">
+                            <button id="cadastrar">Cadastrar</button>
+                            <button id="cancelar">Cancelar</button>
+                        </div>
                     </div>
-                    <div>
-                        <input type="text" placeholder="Senha"/>
-                    </div>
-                    <div>
-                        <button>Cadastrar</button>
-                        <button>Cancelar</button>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </Home>
         </>
     )
