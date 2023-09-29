456373d98c6ab6aef72c19e90882db29f519b871
AfonsoFernando051 <afonsofernando051@gmail.com>
2023-09-29 15:07:34 -0300

    cadastro de usuários funcionando

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Image from 'next/image'
 import styles from './page.module.css'
 import Header from '@/componentes/Header'
@@ -6,6 +7,7 @@ import Sidebar from '@/componentes/Sidebar'
 import Cadastrar from '@/pages/Cadastrar'
 
 export default function Home({children}) {
+
   return (
     <>
       <Header />
--- src/pages/Cadastrar.jsx
+++ src/pages/Cadastrar.jsx
@@ -1,37 +1,81 @@
-import React, {useState} from "react";
+'use client'
+
+import React, { useState } from "react";
 import '../app//styles/globals.css'
 import Header from "@/componentes/Header";
 import Home from "@/app/page";
 import Image from "next/image";
-import { useState } from 'react';
 
 const Cadastrar = function(){
     
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        type: 'e'
+    })
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        })
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        console.log(formData);
+        try{   
+            const response = await fetch('http://localhost:3001/sign-up',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            })
+
+            const data = await response.json()
+            if (response.ok) {
+                alert(data.message);
+            } else {
+                alert(data);
+            }
+
+        }catch(error){
+            console.error('Erro ao cadastrar autoescola', error);
+        }
+    }
+
     return(
         <>
         <Home>
             <div id="cadastrarPag">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div id="titleForm">
                         <h1>Cadastro de autoescolas clientes</h1>
                     </div>
                     <div className="SignUpForm">
                             <h2>Insira os dados de login:</h2>
                         <div>
-                            <input type="text" placeholder="name" className="inputForm"/>
+                            <input type="text" placeholder="Name" className="inputForm" name="name" value={formData.name} onChange={handleChange}/>
                         </div>
                         <div>
-                            <input type="email" placeholder="email" className="inputForm"/>
+                            <input type="email" placeholder="Email" className="inputForm" name="email" value={formData.email} onChange={handleChange}/>
                         </div>
                         <div>
-                            <input type="password" placeholder="password" className="inputForm"/>
+                            <input type="password" placeholder="Password" className="inputForm" name="password" value={formData.password} onChange={handleChange}/>
                         </div>
                         <div>
-                            <input type="radio" name="acess" id="acess" />
+                            <input type="password" placeholder="Confirm password" className="inputForm" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange}/>
+                        </div>
+                        <div>
+                            <input type="radio" name="acess" id="acess"/>
                             <span>Permissão de acesso</span>
                         </div>
                         <div id="buttonForm">
-                            <button id="cadastrar">Cadastrar</button>
+                            <button id="cadastrar" type="submit">Cadastrar</button>
                             <button id="cancelar">Cancelar</button>
                         </div>
                     </div>
