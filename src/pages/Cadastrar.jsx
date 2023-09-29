39e47b099f4c512a41fcf3722d8272aeb551c810
AfonsoFernando051 <afonsofernando051@gmail.com>
2023-09-28 22:44:58 -0300

    começando a implementar cadastro

--- src/pages/Cadastrar.jsx
+++ src/pages/Cadastrar.jsx
@@ -8,7 +8,24 @@ const Cadastrar = function(){
     return(
         <>
         <Home>
-            
+            <form>
+                <h2>Criar Login</h2>
+                <div className="SignUpForm">
+                    <div>
+                        <input type="text" placeholder="Nome"/>
+                    </div>
+                    <div>
+                        <input type="text" placeholder="Email"/>
+                    </div>
+                    <div>
+                        <input type="text" placeholder="Senha"/>
+                    </div>
+                    <div>
+                        <button>Cadastrar</button>
+                        <button>Cancelar</button>
+                    </div>
+                </div>
+            </form>
         </Home>
         </>
     )
